14e7b1bf48d90314ff9c0aa4e718e5f1c997928f
kvarcas91 <e.slutas@gmail.com>
2020-01-05 20:21:57 +0000

    minor styling

--- Core/ViewModels/DashboardViewModel.cs
+++ Core/ViewModels/DashboardViewModel.cs
@@ -3,9 +3,10 @@ using Core.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
-
+using System.Windows.Input;
 
 namespace Core.ViewModels
 {
@@ -16,127 +17,148 @@ namespace Core.ViewModels
 
         public ObservableCollection<Task> Tasks { get; set; }
 
+        public ICommand ShowAddTaskPopupCommand { get; set; }
+
+        public bool AddTaskPopupVisible { get; set; } = false;
+
+        public double TotalProgress { get; set; } = 49;
+
         public DashboardViewModel()
         {
-            Projects = new ObservableCollection<Project> 
-            { 
-                new Project
-                {
-                    Header = "Do something",
-                    Progress = 20,
-                    Priority = Utils.Priority.High,
-                    Content = "First content from Eddie"
-                },
 
-                 new Project
-                 {
-                    Header = "Do More stuff",
-                    Progress = 100,
-                    Priority = Utils.Priority.Medium,
-                    Content = "Second content from Eddie designed to be a long text"
-                 },
+            Projects = new ObservableCollection<Project>();
 
-                  new Project
-                {
-                    Header = "Do something",
-                    Progress = 20,
-                    Priority = Utils.Priority.Low,
-                    Content = "First content from Eddie"
-                },
+            //Projects = new ObservableCollection<Project> 
+            //{ 
+            //    new Project
+            //    {
+            //        Header = "Do something",
+            //        Progress = 20,
+            //        Priority = Utils.Priority.High,
+            //        Content = "First content from Eddie"
+            //    },
 
-                 new Project
-                 {
-                    Header = "Do More stuff",
-                    Progress = 100,
-                    Priority = Utils.Priority.Medium,
-                    Content = "Second content from Eddie designed to be a long text"
-                 },
+            //     new Project
+            //     {
+            //        Header = "Do More stuff",
+            //        Progress = 100,
+            //        Priority = Utils.Priority.Medium,
+            //        Content = "Second content from Eddie designed to be a long text"
+            //     },
 
-                  new Project
-                {
-                    Header = "Do something",
-                    Progress = 20,
-                    Priority = Utils.Priority.High,
-                    Content = "First content from Eddie"
-                },
+            //      new Project
+            //    {
+            //        Header = "Do something",
+            //        Progress = 20,
+            //        Priority = Utils.Priority.Low,
+            //        Content = "First content from Eddie"
+            //    },
 
-                 new Project
-                 {
-                    Header = "Do More stuff",
-                    Progress = 100,
-                    Priority = Utils.Priority.Default,
-                    Content = "Second content from Eddie designed to be a long text"
-                 },
+            //     new Project
+            //     {
+            //        Header = "Do More stuff",
+            //        Progress = 100,
+            //        Priority = Utils.Priority.Medium,
+            //        Content = "Second content from Eddie designed to be a long text"
+            //     },
 
-                  new Project
-                {
-                    Header = "Do something",
-                    Progress = 20,
-                    Priority = Utils.Priority.High,
-                    Content = "First content from Eddie"
-                },
+            //      new Project
+            //    {
+            //        Header = "Do something",
+            //        Progress = 20,
+            //        Priority = Utils.Priority.High,
+            //        Content = "First content from Eddie"
+            //    },
 
-                 new Project
-                 {
-                    Header = "Do More stuff",
-                    Progress = 100,
-                    Priority = Utils.Priority.Medium,
-                    Content = "Second content from Eddie designed to be a long text"
-                 },
+            //     new Project
+            //     {
+            //        Header = "Do More stuff",
+            //        Progress = 100,
+            //        Priority = Utils.Priority.Default,
+            //        Content = "Second content from Eddie designed to be a long text"
+            //     },
 
-                  new Project
-                {
-                    Header = "Do something",
-                    Progress = 20,
-                    Priority = Utils.Priority.High,
-                    Content = "First content from Eddie"
-                },
+            //      new Project
+            //    {
+            //        Header = "Do something",
+            //        Progress = 20,
+            //        Priority = Utils.Priority.High,
+            //        Content = "First content from Eddie"
+            //    },
 
-                 new Project
-                 {
-                    Header = "Do More stuff",
-                    Progress = 99,
-                    Priority = Utils.Priority.Medium,
-                    Content = "Second content from Eddie designed to be a long text"
-                 },
+            //     new Project
+            //     {
+            //        Header = "Do More stuff",
+            //        Progress = 100,
+            //        Priority = Utils.Priority.Medium,
+            //        Content = "Second content from Eddie designed to be a long text"
+            //     },
 
-                  new Project
-                {
-                    Header = "Do something",
-                    Progress = 0,
-                    Priority = Utils.Priority.High,
-                    Content = "First content from Eddie"
-                },
+            //      new Project
+            //    {
+            //        Header = "Do something",
+            //        Progress = 20,
+            //        Priority = Utils.Priority.High,
+            //        Content = "First content from Eddie"
+            //    },
 
-                 new Project
-                 {
-                    Header = "Do More stuff",
-                    Progress = 49,
-                    Priority = Utils.Priority.Medium,
-                    Content = "Second content from Eddie designed to be a long text"
-                 }
-            };
+            //     new Project
+            //     {
+            //        Header = "Do More stuff",
+            //        Progress = 99,
+            //        Priority = Utils.Priority.Medium,
+            //        Content = "Second content from Eddie designed to be a long text"
+            //     },
 
-            Tasks = new ObservableCollection<Task>
-            {
-               new Task
-               {
-                   Content = "First task",
-                   IsCompleted = false
-               },
+            //      new Project
+            //    {
+            //        Header = "Do something",
+            //        Progress = 0,
+            //        Priority = Utils.Priority.High,
+            //        Content = "First content from Eddie"
+            //    },
 
-                new Task
-               {
-                   Content = "Second task",
-                   IsCompleted = false
-               },
+            //     new Project
+            //     {
+            //        Header = "Do More stuff",
+            //        Progress = 49,
+            //        Priority = Utils.Priority.Medium,
+            //        Content = "Second content from Eddie designed to be a long text"
+            //     }
+            //};
 
-                 new Task
-               {
-                   Content = "third task",
-                   IsCompleted = true
-               }
-            };
+
+            Tasks = new ObservableCollection<Task>();
+
+            //Tasks = new ObservableCollection<Task>
+            //{
+            //   new Task
+            //   {
+            //       Content = "First task",
+            //       IsCompleted = false
+            //   },
+
+            //    new Task
+            //   {
+            //       Content = "Second task",
+            //       IsCompleted = false
+            //   },
+
+            //     new Task
+            //   {
+            //       Content = "third task",
+            //       IsCompleted = true
+            //   }
+            //};
+
+            ShowAddTaskPopupCommand = new RelayCommand(ShowAddTaskPopup);
+        }
+
+
+        private void ShowAddTaskPopup ()
+        {
+            AddTaskPopupVisible ^= true;
+            Debug.WriteLine("pop up");
         }
 
     }
