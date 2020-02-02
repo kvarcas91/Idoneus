9cc6843721bcd898f5998c36bd99e132f2cccdb5
kvarcas91 <e.slutas@gmail.com>
2020-02-02 20:47:15 +0000

    update

--- Project/MainWindow.xaml.cs
+++ Project/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using Idoneus.ViewModel;
 using Squirrel;
 using System;
 using System.Diagnostics;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -25,21 +27,30 @@ namespace Idoneus
         {
             Task.Run(async () =>
             {
-                using (var mgr = new UpdateManager(@"https://github.com/kvarcas91/Idoneus"))
+
+
+                try
                 {
-                    var updateInfo = await mgr.CheckForUpdate();
-                    if (updateInfo == null || updateInfo.ReleasesToApply.Count == 0)
+                    using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/kvarcas91/Idoneus"))
                     {
-
-                        return;
-
+                        await mgr.Result.UpdateApp();
+                        var status = mgr.Status;
+                        var result = mgr.Result;
                     }
-
-                    await mgr.DownloadReleases(updateInfo.ReleasesToApply);
-                    await mgr.ApplyReleases(updateInfo);
-
-
                 }
+                catch (Exception ex)
+                {
+                    string message = ex.Message + Environment.NewLine;
+                    if (ex.InnerException != null)
+                        message += ex.InnerException.Message;
+                    Console.WriteLine(message);
+                }
+                //var updateInfo = mgr.CheckForUpdate();
+                // var updateInfo = await mgr.CheckForUpdate();
+                //if (updateInfo.ReleasesToApply.Any())
+                //   ReleaseEntry release = await mgr.UpdateApp();
+                // if (release != null)
+                //UpdateManager.RestartApp();
             });
 
         }
