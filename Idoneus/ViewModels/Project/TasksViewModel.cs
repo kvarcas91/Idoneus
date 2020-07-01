85843c1a8a68c5ad28a36ab3f7088e12ea744afb
kvarcas91 <e.slutas@gmail.com>
2020-07-01 19:08:12 +0100

    init task view

--- Idoneus/ViewModels/Project/TasksViewModel.cs
+++ Idoneus/ViewModels/Project/TasksViewModel.cs
@@ -1,11 +1,87 @@
-﻿using Prism.Mvvm;
+﻿using Common.EventAggregators;
+using Domain.Models.Project;
+using Domain.Models.Tasks;
+using Domain.Repository;
+using Prism.Events;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Idoneus.ViewModels
 {
     public class TasksViewModel : BindableBase
     {
+
+        #region Local members
+
+        private readonly IEventAggregator _eventAggregator;
+        private readonly ProjectRepository _repository;
+
+        #endregion // Local members
+
+        #region Properties
+
+        private Project _currentProject;
+        public Project CurrentProject
+        {
+            get { return _currentProject; }
+            set { SetProperty(ref _currentProject, value); }
+        }
+
+        private ProjectTask _selectedTask;
+        public ProjectTask SelectedTask
+        {
+            get { return _selectedTask; }
+            set { SetProperty(ref _selectedTask, value); OnTaskSelectChanged(); }
+        }
+
+        #endregion
+
+
+        public TasksViewModel(IEventAggregator eventAggregator, ProjectRepository repository)
+        {
+            _eventAggregator = eventAggregator;
+            _repository = repository;
+
+            eventAggregator.GetEvent<SendCurrentProject<Project>>().Subscribe(ProjectReceived);
+        }
+
+        #region Methods
+
+        #region Tasks
+
+        private void OnTaskSelectChanged()
+        {
+            if (SelectedTask == null) return;
+
+
+        }
+
+        #endregion // Tasks
+
+        #region Common
+
+        private void ProjectReceived(Project project)
+        {
+            CurrentProject = project;
+            if (project == null) return;
+
+            Task.Run(() =>
+            {
+                CurrentProject = _repository.GetProject(project.ID);
+            });
+        }
+
+        private void PublishSnackBar(string text)
+        {
+            _eventAggregator.GetEvent<SendSnackBarMessage>().Publish(text);
+        }
+
+        #endregion Common
+
+        #endregion // Methods
+
     }
 }
